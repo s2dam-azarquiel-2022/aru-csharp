bad70e77c58a809eba2ebfb6d1088c4176730c6e
aruZeta <aru_hackZ.official@zohomail.eu>
2022-11-17 19:05:12 +0100

    [refactor] Finished DatosLibros

--- MVVM/DatosLibros/DatosLibros/ViewModel/MainViewModel.cs
+++ MVVM/DatosLibros/DatosLibros/ViewModel/MainViewModel.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Data;
 using System.Data.OleDb;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace DatosLibros.ViewModel
@@ -96,6 +91,61 @@ namespace DatosLibros.ViewModel
             }
         }
 
+        private bool textEnabled;
+        public bool TextEnabled
+        {
+            get => textEnabled;
+            set
+            {
+                textEnabled = value;
+                OnPropertyChanged(nameof(TextEnabled));
+            }
+        }
+
+        private bool buttonsEnabled;
+        public bool ButtonsEnabled
+        {
+            get => buttonsEnabled;
+            set
+            {
+                buttonsEnabled = value;
+                OnPropertyChanged(nameof(ButtonsEnabled));
+            }
+        }
+
+        private bool insertBtnEnabled;
+        public bool InsertBtnEnabled
+        {
+            get => insertBtnEnabled;
+            set
+            {
+                insertBtnEnabled = value;
+                OnPropertyChanged(nameof(InsertBtnEnabled));
+            }
+        }
+
+        private bool editBtnEnabled;
+        public bool EditBtnEnabled
+        {
+            get => editBtnEnabled;
+            set
+            {
+                editBtnEnabled = value;
+                OnPropertyChanged(nameof(EditBtnEnabled));
+            }
+        }
+
+        private string editBtnText;
+        public string EditBtnText
+        {
+            get => editBtnText;
+            set
+            {
+                editBtnText = value;
+                OnPropertyChanged(nameof(EditBtnText));
+            }
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         public ICommand FirstPageCommand { get; set; }
@@ -112,20 +162,28 @@ namespace DatosLibros.ViewModel
         {
             Connection = new OleDbConnection
             {
-                ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source='C:\\Users\\Tu madre\\Downloads\\Librerias.accdb'"
+                ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source='Z:\\MVVM\\DatosLibros\\DatosLibros\\Librerias.accdb'"
             };
-            Adapter = new OleDbDataAdapter("SELECT * FROM libros", Connection);
-            CommandBuilder = new OleDbCommandBuilder();
+            Adapter = new OleDbDataAdapter("SELECT * FROM " + DB_NAME, Connection);
+            CommandBuilder = new OleDbCommandBuilder(Adapter);
             DataSet = new DataSet();
 
             Connection.Open();
-            Adapter.Fill(DataSet, "libros");
+            Adapter.Fill(DataSet, DB_NAME);
             Connection.Close();
 
             PropertyChanged += EventHandler;
 
             CurrentPage = 1;
-            TotalPages = DataSet.Tables[DB_NAME].Rows.Count;
+            UpdateTotalPageCount();
+
+            TextEnabled = false;
+            ButtonsEnabled = true;
+
+            InsertBtnEnabled = false;
+
+            EditBtnEnabled = ButtonsEnabled;
+            EditBtnText = "Modificar";
 
             FirstPageCommand = new Command(FirstPageAction);
             NextPageCommand = new Command(NextPageAction);
@@ -135,11 +193,17 @@ namespace DatosLibros.ViewModel
             InsertBookCommand = new Command(InsertBookAction);
             EditBookCommand = new Command(EditBookAction);
             DeleteBookCommand = new Command(DeleteBookAction);
-            RefreshCommand = new Command(RefreshCAction);
+            RefreshCommand = new Command(RefreshAction);
         }
 
         private void ViewData()
         {
+            if (currentPage < 1)
+            {
+                EmptyFields();
+                return;
+            }
+
             DataRow dataRow = DataSet.Tables[DB_NAME].Rows[CurrentPage - 1];
             if (dataRow.RowState == DataRowState.Deleted) return;
             Title = dataRow["Titulo"].ToString();
@@ -168,16 +232,21 @@ namespace DatosLibros.ViewModel
                     UpdateCurrentPageStr();
                     ViewData();
                     break;
-            }
-        }
 
-        private void UpdateCurrentPageStr()
-        {
-            CurrentPageStr = String.Format("Libro {0} de {1}", CurrentPage, TotalPages);
+                case nameof(ButtonsEnabled):
+                    TextEnabled = !ButtonsEnabled;
+                    break;
+
+                case nameof(InsertBtnEnabled):
+                    ButtonsEnabled = !InsertBtnEnabled;
+                    EditBtnEnabled = ButtonsEnabled;
+                    break;
+            }
         }
 
         private void FirstPageAction(object parameter)
         {
+            if (CurrentPage <= 1) return;
             CurrentPage = 1;
         }
 
@@ -189,33 +258,104 @@ namespace DatosLibros.ViewModel
 
         private void PreviousPageAction(object parameter)
         {
-            if (CurrentPage == 1) return;
+            if (CurrentPage <= 1) return;
             CurrentPage--;
         }
 
         private void LastPageAction(object parameter)
         {
-            CurrentPage = TotalPages;
+            LastPage();
         }
 
         private void NewBookAction(object parameter)
         {
+            EmptyFields();
+            InsertBtnEnabled = true;
+        }
+
+        private void EmptyFields()
+        {
+            Title = "";
+            Isbn = "";
+            Author = "";
+            Editorial = "";
         }
 
         private void InsertBookAction(object parameter)
         {
+            DataRow dataRow = DataSet.Tables[DB_NAME].NewRow();
+            dataRow["Titulo"] = Title;
+            dataRow["ISBN"] = Isbn;
+            dataRow["Autor"] = Author;
+            dataRow["Editorial"] = Editorial;
+            DataSet.Tables[DB_NAME].Rows.Add(dataRow);
+            UpdateTotalPageCount();
+            LastPage();
+            InsertBtnEnabled = false;
         }
 
         private void EditBookAction(object parameter)
         {
+            ButtonsEnabled = !ButtonsEnabled;
+            if (ButtonsEnabled)
+            {
+                DataRow dataRow = DataSet.Tables[DB_NAME].Rows[CurrentPage-1];
+                dataRow.BeginEdit();
+                dataRow["Titulo"] = Title;
+                dataRow["ISBN"] = Isbn;
+                dataRow["Autor"] = Author;
+                dataRow["Editorial"] = Editorial;
+                dataRow.EndEdit();
+                EditBtnText = "Modificar";
+            } else
+            {
+                EditBtnText = "Aceptar";
+            }
         }
 
         private void DeleteBookAction(object parameter)
         {
+            if (CurrentPage < 1) return;
+            DataSet.Tables[DB_NAME].Rows[CurrentPage - 1].Delete();
+            Refresh();
         }
 
-        private void RefreshCAction(object parameter)
+        private void RefreshAction(object parameter)
         {
+            Refresh();
+        }
+
+        private void Refresh()
+        {
+            Connection.Open();
+            Adapter.Update(DataSet, DB_NAME);
+            Connection.Close();
+            UpdateTotalPageCount();
+            CheckCurrentPage();
+        }
+
+        private void LastPage()
+        {
+            CurrentPage = TotalPages;
+        }
+
+        private void UpdateTotalPageCount()
+        {
+            TotalPages = DataSet.Tables[DB_NAME].Rows.Count;
+        }
+
+        private void CheckCurrentPage()
+        {
+            CurrentPage = CurrentPage > TotalPages ? TotalPages : CurrentPage;
+        }
+
+        private void UpdateCurrentPageStr()
+        {
+            CurrentPageStr = String.Format(
+                "Libro {0} de {1}",
+                CurrentPage,
+                TotalPages
+            );
         }
     }
 }
