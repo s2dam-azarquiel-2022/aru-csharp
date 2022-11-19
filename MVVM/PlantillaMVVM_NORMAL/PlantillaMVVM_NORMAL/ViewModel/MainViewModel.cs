90ddd3f5ed094a2f9acb8131e57b7551e6e53e0d
aruZeta <aru_hackZ.official@zohomail.eu>
2022-11-19 11:13:36 +0100

    [add] Project templates

---
+++ MVVM/PlantillaMVVM_NORMAL/PlantillaMVVM_NORMAL/ViewModel/MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PlantillaMVVM_NORMAL.ViewModel
+{
+    internal class MainViewModel : INotifyPropertyChanged
+    {
+        public MainViewModel()
+        {
+            // Setup the event handler
+            PropertyChanged += EventHandler;
+
+            // Setup commands
+            // ...
+
+            // Setup other stuff
+            // ...
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(
+                this,
+                new PropertyChangedEventArgs(propertyName)
+            );
+        }
+
+        private void EventHandler(object? sender, PropertyChangedEventArgs args)
+        {
+            switch (args.PropertyName)
+            {
+            }
+        }
+    }
+}
